b70b14074c2c4713e63a82a297c1321a4ca50a39
miesiwo <sachok.vlad.vitalevich@gmail.com>
2023-12-05 11:45:51 +0200

    sec

---
+++ src/Timer.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(prevSeconds => prevSeconds + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const remainingSeconds = time % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  };
+
+  return (
+    <div>
+      <h1>Таймер</h1>
+      <p>Пройшло часу: {formatTime(seconds)}</p>
+    </div>
+  );
+};
+
+export default Timer;
